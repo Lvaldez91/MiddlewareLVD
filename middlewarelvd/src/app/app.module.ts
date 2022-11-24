09fade41d01c2631911e9377977fa5c97c5f5ee6
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-11-23 22:08:02 -0600

    add structure

--- middlewarelvd/src/app/app.module.ts
+++ middlewarelvd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { MainComponent } from './components/shared/main/main.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
